aee36097e48f448e76cc5396e1c494a0ddb3fc50
William Koller <williamkoller404@gmail.com>
2021-12-30 17:44:49 -0300

    chore: initial commit with setup

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@/modules/app/app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const app = await NestFactory.create(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get<string>('port');
+  const nodeEnv = config.get<string>('nodeEnv');
+  await app.listen(port, () =>
+    logger.log(`server is running in ${nodeEnv} mode`),
+  );
+}
+bootstrap();
