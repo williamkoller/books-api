d18993d307f10750b44faddc8294feff703e8871
William Koller <williamkoller404@gmail.com>
2021-12-31 16:40:49 -0300

    feat: add booksTransformers

---
+++ src/utils/transformer/books/books.transformer.ts
@@ -0,0 +1,17 @@
+import { BookEntity } from '@/infra/typeorm/entities/book-entity/book.entity';
+import { BooksTypeOutput } from '@/utils/types/books/books.type';
+
+export const booksTransformers = (books: BookEntity[]): BooksTypeOutput[] => {
+  return books.map((book) => ({
+    id: book.id,
+    author: book.author,
+    title: book.title,
+    state: book.state,
+    price: book.price,
+    pages: book.pages,
+    release_year: book.release_year,
+    publishing_company: book.publishing_company,
+    createdAt: book.createdAt,
+    updatedAt: book.updatedAt,
+  }));
+};
