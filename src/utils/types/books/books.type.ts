c0be18c1e76cfdc4c4e65c5d940ca446b2836630
William Koller <williamkoller404@gmail.com>
2021-12-31 16:40:38 -0300

    feat: add BooksTypeOutput

---
+++ src/utils/types/books/books.type.ts
@@ -0,0 +1,12 @@
+export type BooksTypeOutput = {
+  id: string;
+  author: string;
+  title: string;
+  state: string;
+  price: number;
+  pages: number;
+  release_year: string;
+  publishing_company: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
