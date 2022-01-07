c39f6ccc0a08346d436843026dd5490e186e7595
William Koller <williamkoller404@gmail.com>
2022-01-07 01:16:59 -0300

    refactor: add NotFoundException in case array returns empty on LoadAllBooksService

--- src/modules/books/services/load-all-books/load-all-books.service.ts
+++ src/modules/books/services/load-all-books/load-all-books.service.ts
@@ -1,6 +1,6 @@
 import { BookEntity } from '@/infra/typeorm/entities/book-entity/book.entity';
 import { booksTransformers } from '@/utils/transformer/books/books.transformer';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { BooksRepository } from '@/modules/books/repositories/books.repository';
 
 @Injectable()
@@ -9,6 +9,11 @@ export class LoadAllBooksService {
 
   public async loadAllBooks(): Promise<BookEntity[]> {
     const books = await this.booksRepo.loadAllBooks();
+
+    if (!books.length) {
+      throw new NotFoundException('no record found.');
+    }
+
     return booksTransformers(books);
   }
 }
