0bfbe73fdb1cdb46f36efca985e8ebfd1b992b99
William Koller <williamkoller404@gmail.com>
2022-01-15 23:20:48 -0300

    refactor: alter field implement books text array

--- src/infra/typeorm/migrations/1642254598765-CreateTableCategories.ts
+++ src/infra/typeorm/migrations/1642254598765-CreateTableCategories.ts
@@ -6,13 +6,10 @@ export class CreateTableCategories1642254598765 implements MigrationInterface {
         "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
         "name" character varying NOT NULL,
         "description" character varying NOT NULL,
-        "book_id" uuid,
+        "books" text ARRAY,
         created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT "categories_pk_id" PRIMARY KEY ("id"),
-        CONSTRAINT "fk_book_id_key" FOREIGN KEY ("book_id")
-        REFERENCES "books"("id") ON DELETE CASCADE ON UPDATE CASCADE
-        );`);
+        CONSTRAINT "categories_pk_id" PRIMARY KEY ("id"));`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
