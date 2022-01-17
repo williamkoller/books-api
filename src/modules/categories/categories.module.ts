5d18244417017fba37b2cbca3a73597472610e6f
William Koller <williamkoller404@gmail.com>
2022-01-17 00:12:38 -0300

    feat: add imports with TypeOrmModule

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,4 +1,8 @@
+import { CategoryEntity } from '@/infra/typeorm/entities/category-entity/category.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([CategoryEntity])],
+})
 export class CategoriesModule {}
